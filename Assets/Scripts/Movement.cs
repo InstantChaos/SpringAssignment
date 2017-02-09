2d208af2c3724c55fbd331e2981a4004a4dbe626
fchong5 <300649025@cencol.ca>
2017-02-09 13:27:16 -0500

    initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    float posX;
+    float posY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       posX =  Input.mousePosition.x;
+       posY = Input.mousePosition.y;
+    }
+    void OnMouseDrag()
+    {
+         transform.position = Camera.main.ScreenToWorldPoint(new Vector3(posX, posY, 10.0f));
+    }
+}
